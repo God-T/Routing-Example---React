8c150bb48bd681b6fe60add8246a7accd205ed96
Shuyu <s.zong@student.unsw.edu.au>
2020-06-20 22:03:29 +1000

    Navigation

--- src/components/productDetails.jsx
+++ src/components/productDetails.jsx
@@ -3,6 +3,8 @@ import React, { Component } from "react";
 class ProductDetails extends Component {
   handleSave = () => {
     // Navigate to /products
+    // this.props.history.replace("/products"); //no history; cannot be back; used in login page
+    this.props.history.push("/products"); //push to history; can be back
   };
 
   render() {
