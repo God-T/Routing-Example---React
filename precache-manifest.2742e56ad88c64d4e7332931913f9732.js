99725c21bc0b0be21fee50aceb805f1da4c10319
Shuyu <s.zong@student.unsw.edu.au>
2020-06-20 22:35:19 +1000

    Updates

---
+++ precache-manifest.2742e56ad88c64d4e7332931913f9732.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "109a8500f15042c818a87cdbbe23dbd9",
+    "url": "/Routing-Example-React/index.html"
+  },
+  {
+    "revision": "c4942ddd93f99a64e2fa",
+    "url": "/Routing-Example-React/static/css/main.716df5c6.chunk.css"
+  },
+  {
+    "revision": "4eb2506f163a7dafdeb6",
+    "url": "/Routing-Example-React/static/js/2.f4e6651b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Routing-Example-React/static/js/2.f4e6651b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4942ddd93f99a64e2fa",
+    "url": "/Routing-Example-React/static/js/main.a9f1ea98.chunk.js"
+  },
+  {
+    "revision": "850620947571d632e76b",
+    "url": "/Routing-Example-React/static/js/runtime-main.97ac637d.js"
+  }
+]);
